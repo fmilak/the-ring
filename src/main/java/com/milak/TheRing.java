7e76edfc24eee597235173b42f535e97cb6c22e0
Filip <fmilak22@gmail.com>
2019-10-19 18:20:06 +0200

    initial config

---
+++ src/main/java/com/milak/TheRing.java
@@ -0,0 +1,14 @@
+package com.milak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRing {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRing.class);
+    }
+
+}
+
