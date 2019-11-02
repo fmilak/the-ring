576f9829dbfc3e9202c8de0bcf0415e560f4c164
filip <fmilak22@gmail.com>
2019-11-02 12:06:44 +0100

    flyway works

--- src/main/resources/db/migration/V0.1__init.sql
+++ src/main/resources/db/migration/V0.1__init.sql
@@ -1,6 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS dev;
-
-create table dev.user
+create table user
 (
 	uuid varchar2(255) not null,
 	password varchar2(255) not null,
@@ -10,11 +8,11 @@ create table dev.user
 );
 
 create unique index user_uuid_uindex
-	on dev.user (uuid);
+	on user (uuid);
 
-alter table dev.user
+alter table user
 	add constraint user_pk
 		primary key (uuid);
 
-insert into dev.user (uuid, password, name, surname, email)
+insert into user (uuid, password, name, surname, email)
     values ('4d2793c4-6550-4741-bb12-7c997597d854', 'admin', 'admin', 'admin', 'admin');
