20c8fdd3ce9d8928a72ce5d3f35157be80ab16d4
Filip <filip.milak@pinecone.hr>
2019-11-13 15:53:54 +0100

    jos neke dorade

--- front/the-ring/src/AppRouter.tsx
+++ front/the-ring/src/AppRouter.tsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Route, Router, Switch} from "react-router-dom";
 import LoginView from "./login/LoginView";
 import {createBrowserHistory} from 'history';
 import ProfileView from "./layout/profile/ProfileView";
 import HomeView from "./layout/HomeView";
-import {Layout, Menu} from "antd";
+import {Icon, Layout, Menu} from "antd";
+import {RootStoreContext} from "./App";
 
 const { Header, Content } = Layout;
 
 const customHistory = createBrowserHistory();
 
 const AppRouter = () => {
+    const rootStore = useContext(RootStoreContext);
+    const {loginStore} = rootStore;
+
     return (
         <Router history={customHistory}>
             <div>
-                <Layout className="layout">
+                <Layout className="layout" style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
                     <Header>
                         <div className="logo" />
                         <Menu
@@ -23,21 +27,23 @@ const AppRouter = () => {
                             defaultSelectedKeys={['1']}
                             style={{ lineHeight: '64px' }}
                         >
-                            <Menu.Item key="1" onClick={() => customHistory.push("/")}>Home</Menu.Item>
-                            <Menu.Item key="2" onClick={() => customHistory.push("/login")}>Login</Menu.Item>
-                            <Menu.Item key="3" onClick={() => customHistory.push(`/filip`)}>Profile</Menu.Item>
+                            <Menu.Item key="1" onClick={() => customHistory.push("/")}><Icon type="home" />Home</Menu.Item>
+                            <Menu.Item key="2" onClick={() => customHistory.push("/login")}><Icon type="home" />Login</Menu.Item>
+                            <Menu.Item key="3" onClick={() => customHistory.push(`/${loginStore.username}`)}><Icon type="user" />Profile</Menu.Item>
                         </Menu>
                     </Header>
                     <Content style={{ padding: '0 50px' }}>
-                        <Switch>
-                            <Route path="/login">
-                                <LoginView />
-                            </Route>
-                            <Route path="/:profileName" children={<ProfileView/>} />
-                            <Route path="/">
-                                <HomeView />
-                            </Route>
-                        </Switch>
+                        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                            <Switch>
+                                <Route path="/login">
+                                    <LoginView />
+                                </Route>
+                                <Route path="/:profileName" children={<ProfileView/>} />
+                                <Route path="/">
+                                    <HomeView />
+                                </Route>
+                            </Switch>
+                        </div>
                     </Content>
                 </Layout>
             </div>
--- front/the-ring/src/layout/profile/ProfileView.tsx
+++ front/the-ring/src/layout/profile/ProfileView.tsx
@@ -1,13 +1,23 @@
-import React, {ReactElement} from "react";
+import React, {useContext} from "react";
 import {useParams} from "react-router";
+import {RootStoreContext} from "../../App";
+import {Redirect, Route, useHistory} from "react-router-dom";
 
-const ProfileView = (): ReactElement => {
+const ProfileView = () => {
     const {profileName} = useParams();
-
+    const rootStore = useContext(RootStoreContext);
+    const {loginStore} = rootStore;
+    if (loginStore.username === profileName) {
+        return (
+            <div>
+                <h3>{profileName}</h3>
+            </div>
+        )
+    }
     return (
-        <div>
-            <h3>{profileName}</h3>
-        </div>
+        <Route>
+            <Redirect to="/" />
+        </Route>
     )
 };
 
