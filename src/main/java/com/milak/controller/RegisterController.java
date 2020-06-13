4fd7a1f9706683813f26dbc96986b3588a2725d7
Filip <fmilak22@gmail.com>
2020-06-13 10:33:29 +0200

    integration tests

--- src/main/java/com/milak/controller/RegisterController.java
+++ src/main/java/com/milak/controller/RegisterController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/sign-in/")
+@RequestMapping("/api/sign-in")
 public class RegisterController {
 
     @Autowired
